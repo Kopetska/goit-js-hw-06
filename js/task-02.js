2ccabd7ff1879fd4204ef2dfa80a9f50c7f255bd
anna <a.laptieva@icloud.com>
2022-03-25 17:43:07 +0200

    Add second task

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,42 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const firstItem = document.createElement("li");
+firstItem.classList.add("item");
+firstItem.textContent = "Potatoes";
+
+const secondItem = document.createElement("li");
+secondItem.classList.add("item");
+secondItem.textContent = "Mushrooms";
+
+const thirdItem = document.createElement("li");
+thirdItem.classList.add("item");
+thirdItem.textContent = "Garlic";
+
+const fifthItem = document.createElement("li");
+fifthItem.classList.add("item");
+fifthItem.textContent = "Tomatos";
+
+const sixthItem = document.createElement("li");
+sixthItem.classList.add("item");
+sixthItem.textContent = "Herbs";
+
+const seventhItem = document.createElement("li");
+seventhItem.classList.add("item");
+seventhItem.textContent = "Condiments";
+
+const total = document.querySelector("ul");
+total.append(
+  fifthItem,
+  secondItem,
+  thirdItem,
+  fifthItem,
+  sixthItem,
+  seventhItem
+);
